be6ba4cc22e457a79e03d8826bd35149d56e8da4
saurav sanjel <jobarsanjel2@gmail.com>
2023-10-23 15:39:59 +0530

    database uri change

--- backend/src/bin/database.ts
+++ backend/src/bin/database.ts
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
 export const ConnectToDatbase = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  await mongoose.connect(process.env.MONGODB_URI);
 };
